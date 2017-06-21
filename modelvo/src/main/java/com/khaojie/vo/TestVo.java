592bc6a378550492ebe047fdb06ea95ed841d138
khaojie <khaojie@192.168.0.101>
2017-06-21 11:55:56 +0800

    first commit

--- modelvo/src/main/java/com/khaojie/vo/TestVo.java
+++ modelvo/src/main/java/com/khaojie/vo/TestVo.java
@@ -1,8 +1,10 @@
 package com.khaojie.vo;
 
+import com.khaojie.pojo.Product;
+
 /**
  * Created by khaojie on 2017/6/20.
  */
 public class TestVo {
-//    private Product product;
+    private Product product;
 }
